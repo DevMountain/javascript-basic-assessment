3ffbbac4e623aaccadb387220f92a345d271e71f
Donovan Hiland <donovan.hiland@gmail.com>
2016-08-11 12:31:25 -0600

    finished basics retake

--- assessment.js
+++ assessment.js
@@ -1,91 +1,93 @@
-// #1 Create a var called hello that says goodbye
+// #1 Create a var called 'up' that says 'down'
 
 
 
-// #2 Use iLove to create a new var called iLoveCode that says "I love code"
-var iLove = 'I love';
+// #2 Use the var iHeart to create a new var called iHeartJavascript that says 'I heart javascript'
+var iHeart = 'I heart';
 
-// #3  Make an object called bob
-// bob has a height of 6ft (string)
-// bob has an age of 24 (Number)
-// bob has hair, that has style spikey, and color brown (object)
-// bob is not presidentOfTheUnitedStates (boolean)
-// bob likes apples, bananas, and cherries (array of strings)
+// #3  Make an object called developer
+// developer has a title of Junior (String)
+// developer has a salary of 55000 (Number)
+// developer has a laptop, that has type Macbook, and year 2016 (object)
+// developer is not billGates (boolean)
+// developer likes isolation, caffeine, and pizza (array of strings)
 
 
-// #4 Change my shirt color to pink using dot notation
-var myShirt = {
-	type: 'polo',
-	color: 'red'
+// #4 Change my dog's name to Fido using dot notation
+var myDog = {
+	type: 'beagle',
+	name: 'Spot'
 };
 
-// Change my shirt type to spandex using square bracket notation
-var myOtherShirt = {
-	type: 'polo',
-	color: 'red'
+// Change my don't type to hairless using square bracket notation
+var myOtherDog = {
+	type: 'poodle',
+	name: 'Fluffy'
 };
 
-// #5 Create an object that tracks a count of animals in a zoo.  Call it 'zoo'
-// The key should be the animal name(string) and the value should be how many there are.
-// Our zoo has 8 monkeys, 4 giraffe’s and 2 elephants
+// #5 Create an object that tracks a count of kids in each grade in a school.  Call it 'school'
+// The key should be the grade (string) and the value should be how many kids there are.
+// Our school has 50 kids in first, 30 kids in second and 80 kids in third
 
 
 
-// #6 Loop through this object and change all properties that start with the letter s to have a value of 's'
-var snake = {
-	sliters: 'sideways',
-	eats: 'rodents',
-	says: 'ssss',
-	smells: 'heat',
-	runs: 'legless'
+// #6 Loop through this object and change all properties that start with the letter m to have a value of 'm'
+var cow = {
+	eats: 'grass',
+  attitude: 'mosey',
+	says: 'mooooo',
+	cares: false,
+  produces: 'milk',
 };
 
 
-// #7 Create an array of strings that are the 6 primary colors (lower-case). Call your array primaryColors
+// #7 Create an array of strings that are the 7 days of the week. Call your array daysOfWeek
 
 // #8 Using this array do the following
-var heroes = ['superman', 'batman', 'flash'];
-// add 'wonderwoman' to the end
+var jedi = ['Yoda', 'Mace Windu', 'Obi-Wan'];
+// add 'Luke' to the end
 
-// remove 'superman' and store him in a var called firstHero
+// remove 'Yoda' and store him in a var called firstJedi
 
-// add 'spongebob' to the start of the array
+// add 'Mario' to the start of the array
 
-// remove 'flash' from the array and store him in a var called secondHero
+// remove 'Obi-Wan' from the array and store him in a var called secondJedi
 
-// leave batman in the array but put a copy of him on a var called thirdHero
-
-
+// leave 'Mace Windu' in the array but put a copy of him on a var called thirdJedi
 
 
-// #9 Write a function called addItem, that takes in an array, and adds the item to
-// the array.
 
 
-// #10 Write a function called removeItem, that takes in an array of strings, and a string.
-// Removes all instances of that string from the array. And return the modified array.
+// #9 Write a function called cutInLine, that takes in an array and an item, and adds the item
+// after the first item in the array and return the array
+// EX: cutInLine([1,2,3,4], 8) => [1,8,2,3,4];
+
+
+// #10 Write a function called removeThings, that takes in an array of things (strings or numbers), and a 'thing' (a string or a number).
+// Removes all instances of that 'thing' from the array. And return the modified array.
 // The order of the array should not be changed
+// removeThings(['a', 1, 4, 'a'], 'a') => [1,4]
 
 
-// #11 Write a function doubleTheFun that takes 1 parameter it doubles numbers, and
-// repeats strings example 4->8, 2.5->5, 'Awesome'->'AwesomeAwesome'
+// #11 Write a function tripleTheFun that takes 1 parameter: a number or a string. It triples numbers, and
+// repeats strings. Example: 4->12, 2.5->7.5, 'Awesome'->'AwesomeAwesomeAwesome'
 
 
-// #12 Write function getValueOfProperty that takes in an object, and the name of a property on the object
+// #12 Write function getPropertyValue that takes in an object, and the name of a property on the object as a string
 // return the value from the object that corresponds to the property
 
-// #13 Write a function called makeChatMessage that takes in a message and author parameter
-// and returns an object with a message, author, and timestamp, that is
+// #13 Write a function called clockIn that takes in an employee and department name
+// and returns an object with a name, department, and timestamp which is
 // the current time as a Date object
 
-// #14 Create a function called coderTest that takes in an object that is a person, it looks to see if the person’s name is Jeremy and changes the person object to have a property called lovesCode with a value of 10.  If their name is Brack set lovesCode to 0.  otherwise set lovesCode to 5.
+// #14 Create a function called spiderTest that takes in an object that is an actor, it looks to see if the actor's name is Andrew Garfield and changes the actor object to have a property called spiderRating with a value of 10.  If their name is Tobey Maguire set spiderRating to 0.  otherwise set spiderRating to 'not a spiderman'.
 
-// #15 Create a function called outside that takes in a temperature (number), a humidity(number), and a cloudiness(number), in that order. Using the following to return the correct values
+// #15 Create a function called whatIsItOutside that takes in a temperature (number), a humidity(number), and a cloudiness(number), in that order. Using the following to return the correct values
 /*
-    temperature over 80 and humidity over 40 - return "I'm all sweat"
-    temperature under 40 and cloudiness over 60 - return "I have icecicles"
-    temperature over 80 and humidity under 40 and cloudiness under 20 - return "I'm literally in the desert"
-    temperature over 80 or humidity over 50 or cloudiness over 50 - return "Hmm, probably not"
+    temperature over 85 and humidity over 45 - return "I'm all sweat"
+    temperature under 40 and cloudiness over 60 - return "I have icicles"
+    temperature over 85 and humidity under 45 and cloudiness under 20 - return "I'm literally in the desert"
+    temperature over 90 or humidity over 55 or cloudiness over 65 - return "Hmm, probably not"
     Otherwise - return "I love outside"
 */
 
